fc4364764add79ca30e266e263014792d99fa4a7
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-03-24 13:15:15 +0900

    通知メッセージを変更

--- function/index.py
+++ function/index.py
@@ -123,7 +123,7 @@ def create_message(total_billing: dict, service_billings: list) -> Tuple[str, st
 
     total = round(float(total_billing["billing"]), 2)
 
-    title = f"AWS請求額通知({start}～{end_yesterday}):{total:.2f} USD"
+    title = f"AWS請求額通知({start}～{end_yesterday}) : {total:.2f} USD"
 
     details = []
     for item in service_billings:
